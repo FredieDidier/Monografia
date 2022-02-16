7eee8851e2ae53984b5c3ed3051fd9ac5b6eb403
FredieDidier <fdidier@terra.com.br>
2022-02-16 08:56:07 -0300

    Update Matriz de Transição.R

--- pnad_painel/pnadcontinua/Matriz de Transição.R
+++ pnad_painel/pnadcontinua/Matriz de Transição.R
@@ -2,14 +2,14 @@
 
 df = data_2019 %>%
   mutate(position = case_when(workforce_condition == 2 ~ 1,
-                         worker == 2 ~ 2,
+                         worker == 2 & occupation_condition == 2 & workforce_condition == 1 ~ 2,
                          worker == 1 & signed_work_card == 2 ~ 3,
                          worker == 1 & signed_work_card == 1 ~ 4,
                          worker == 1 & job_function == 6 ~ 5,
                          worker == 1 & job_function == 5 ~ 6,
                          worker == 1 & job_function == 4 ~ 7)) %>%
   select(id_code, year_quarter, workforce_condition, worker, signed_work_card,
-         job_function, position)
+         job_function, occupation_condition, position)
 
 funcao = function(df, initial_quarter, final_quarter, initial_position,
                   final_position){
@@ -31,8 +31,7 @@ funcao = function(df, initial_quarter, final_quarter, initial_position,
   final_number/initial_number
 }
 
- funcao(df, "2019_3", "2019_4", 7, 6)
-
+ funcao(df, "2019_3", "2019_4", 7, 2)
 
 ####
 
@@ -56,13 +55,13 @@ matriz1 = cria_matriz_transicao("2019_1", "2019_2")
 
 
 ######### Linhas/Colunas da Matriz ####
-# Linha 1: 63%, 5,4%, 1,8%, 1,3%, 2,3%, 0,1%, 0,07%
-# Linha 2: 18%, 31%, 7,5%, 5,3%, 8%, 0,4%, 1,2%
-# Linha 3: 7%, 7,5%, 42%, 7%, 6,3%, 0,63%, 1,5%
-# Linha 4: 2%, 3,2%, 3%, 61%, 1,2%, 0,3%, 1%
-# Linha 5: 7,3%, 6,2%, 4,8%, 1,6%, 52%, 2,2%, 0,1%
-# Linha 6: 2,2%, 2%, 2,8%, 2,2%, 14%, 50%, 0,2%
-# Linha 7: 1,2%, 2,7%, 3,8%, 4,5%, 0,4%, 0,12%, 61%   
+# Linha 1: 63%, 3,4%, 1,8%, 1,3%, 2,3%, 0,1%, 0,07%
+# Linha 2: 19,3%, 29%, 7,3%, 5%, 5,3%, 0,1%, 0,13%
+# Linha 3: 7%, 3,9%, 42%, 7%, 6,3%, 0,63%, 1,5%
+# Linha 4: 2%, 1,7%, 3%, 61%, 1,2%, 0,3%, 1%
+# Linha 5: 7,3%, 2%, 4,8%, 1,6%, 52%, 2,2%, 0,1%
+# Linha 6: 2,2%, 0,4%, 2,8%, 2,2%, 14%, 50%, 0,2%
+# Linha 7: 1,2%, 0,18%, 3,8%, 4,5%, 0,4%, 0,12%, 61%   
 
 ################
 ### Diagonais da Matriz ####
