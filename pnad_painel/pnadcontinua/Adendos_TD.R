b4fd32bcccf2f2eb202a8772a35381692ed30413
FredieDidier <fdidier@terra.com.br>
2022-02-22 13:11:02 -0300

    Update Adendos_TD.R

--- pnad_painel/pnadcontinua/Adendos_TD.R
+++ pnad_painel/pnadcontinua/Adendos_TD.R
@@ -1,14 +1,14 @@
 
 ## Checando número de mulheres
 
-df = data_2019 %>%
+dat = data_2019 %>%
   filter(gender == 2) %>%
-  select(gender) 
+  select(id_code, gender, weights) 
 #########################################
 
 ## Checando número de negros
 
-df = data_2019 %>%
+dat = data_2019 %>%
   mutate(negro = case_when(race == 2 | race == 4 | race == 5 ~ 1,
                            race == 1 | race == 3 ~ 0)) %>%
   filter(negro == 1) %>%
@@ -17,19 +17,19 @@ df = data_2019 %>%
 
 ## Descobrindo o número de trabalhadores formais pela ocupação
 
-df = data_2019 %>%
+dat = data_2019 %>%
   filter(job_function %in% c(3) & worker == 1 & signed_work_card == 1) %>%
   select(job_function, worker, signed_work_card)
 
-df = data_2019 %>%
+dat = data_2019 %>%
   filter(job_function %in% c(4) & worker == 1 & signed_work_card == 1) %>%
   select(job_function, worker, signed_work_card)
 
-df = data_2019 %>%
+dat = data_2019 %>%
   filter(job_function %in% c(5) & worker == 1 & cnpj == 1) %>%
   select(job_function, worker, social_security_taxpayer)
 
-df = data_2019 %>%
+dat = data_2019 %>%
   filter(job_function %in% c(6) & worker == 1 & social_security_taxpayer == 1) %>%
   select(job_function, worker, social_security_taxpayer)
 
@@ -37,19 +37,19 @@ df = data_2019 %>%
 
 ## Descobrindo o número de trabalhadores informais pela ocupação
 
-df = data_2019 %>%
+dat = data_2019 %>%
   filter(job_function %in% c(3) & worker == 1 & signed_work_card == 2) %>%
   select(job_function, worker, signed_work_card)
 
-df = data_2019 %>%
+dat = data_2019 %>%
   filter(job_function %in% c(4) & worker == 1 & signed_work_card == 2) %>%
   select(job_function, worker, signed_work_card)
 
-df = data_2019 %>%
+dat = data_2019 %>%
   filter(job_function %in% c(5) & worker == 1 & cnpj == 2) %>%
   select(job_function, worker, social_security_taxpayer)
 
-df = data_2019 %>%
+dat = data_2019 %>%
   filter(job_function %in% c(6) & worker == 1 & social_security_taxpayer == 2) %>%
   select(job_function, worker, social_security_taxpayer)
 
@@ -57,7 +57,7 @@ df = data_2019 %>%
 
 ## Descobrindo o número de inativos
 
-df = data_2019 %>%
+dat = data_2019 %>%
   filter(workforce_condition == 2) %>%
   select(workforce_condition)
 ##############################
@@ -65,213 +65,213 @@ df = data_2019 %>%
 ## Descobrindo idade média de todos os grupos formais + inativos
 
 # Inativos
-df = data_2019 %>%
+dat = data_2019 %>%
   filter(workforce_condition == 2) %>%
   select(workforce_condition, age)
 
-a = mean(df$age)
-aa = sd(df$age)
+a = mean(dat$age)
+aa = sd(dat$age)
 
 # Setor Privado
-df = data_2019 %>%
+dat = data_2019 %>%
   filter(job_function %in% c(3) & worker == 1 & signed_work_card == 1) %>%
   select(job_function, worker, signed_work_card, age)
 
-b = mean(df$age)
-bb = sd(df$age)
+b = mean(dat$age)
+bb = sd(dat$age)
 
 # Setor Público
-df = data_2019 %>%
+dat = data_2019 %>%
   filter(job_function %in% c(4) & worker == 1 & signed_work_card == 1) %>%
   select(job_function, worker, signed_work_card, age)
 
-c = mean(df$age)
-cc = sd(df$age)
+c = mean(dat$age)
+cc = sd(dat$age)
 
 # Empregadores 
 
-df = data_2019 %>%
+dat = data_2019 %>%
   filter(job_function %in% c(5) & worker == 1 & cnpj == 1) %>%
   select(job_function, worker, cnpj, age)
 
-d = mean(df$age)
-dd = sd(df$age)
+d = mean(dat$age)
+dd = sd(dat$age)
 
 # Conta Própria
-df = data_2019 %>%
+dat = data_2019 %>%
   filter(job_function %in% c(6) & worker == 1 & social_security_taxpayer == 1) %>%
   select(job_function, worker, social_security_taxpayer, age)
 
-e = mean(df$age)
-ee = sd(df$age)
+e = mean(dat$age)
+ee = sd(dat$age)
 ########################################
 
 # Descobrindo idade média de todos os grupos informais
 
-df = data_2019 %>%
+dat = data_2019 %>%
   filter(job_function %in% c(3) & worker == 1 & signed_work_card == 2) %>%
   select(job_function, worker, signed_work_card, age)
 
-f = mean(df$age)
-ff = sd(df$age)
+f = mean(dat$age)
+ff = sd(dat$age)
 
-df = data_2019 %>%
+dat = data_2019 %>%
   filter(job_function %in% c(4) & worker == 1 & signed_work_card == 2) %>%
   select(job_function, worker, signed_work_card, age)
 
-g = mean(df$age)
-gg = sd(df$age)
+g = mean(dat$age)
+gg = sd(dat$age)
 
-df = data_2019 %>%
+dat = data_2019 %>%
   filter(job_function %in% c(5) & worker == 1 & cnpj == 2) %>%
   select(job_function, worker, cnpj, age)
 
-h = mean(df$age)
-hh = sd(df$age)
+h = mean(dat$age)
+hh = sd(dat$age)
 
-df = data_2019 %>%
+dat = data_2019 %>%
   filter(job_function %in% c(6) & worker == 1 & social_security_taxpayer == 2) %>%
   select(job_function, worker, social_security_taxpayer, age)
 
-i = mean(df$age)
-ii = sd(df$age)
+i = mean(dat$age)
+ii = sd(dat$age)
 ##########################
 
 ## Descobrindo anos de estudo médio dos inativos e das ocupações e salário médio
 
 # Inativos
-df = data_2019 %>%
+dat = data_2019 %>%
   filter(workforce_condition == 2) %>%
   select(workforce_condition, years_of_study)
 
-j = mean(df$years_of_study)
-jj = sd(df$years_of_study)
+j = mean(dat$years_of_study)
+jj = sd(dat$years_of_study)
 
 # Setor Privado Formal e Informal
 
-df = data_2019 %>%
+dat = data_2019 %>%
   filter(job_function %in% c(3) & worker == 1 & signed_work_card == 1) %>%
   select(job_function, worker, signed_work_card, years_of_study)
 
-k = mean(df$years_of_study)
-kk = sd(df$years_of_study)
+k = mean(dat$years_of_study)
+kk = sd(dat$years_of_study)
 
-df = data_2019 %>%
+dat = data_2019 %>%
   filter(job_function %in% c(3) & worker == 1 & signed_work_card == 1 &
            !is.na(monthly_work_income)) %>%
   select(job_function, worker, signed_work_card, monthly_work_income)
 
-kkk = mean(df$monthly_work_income)
-kkkk = sd(df$monthly_work_income)
+kkk = mean(dat$monthly_work_income)
+kkkk = sd(dat$monthly_work_income)
 
-df = data_2019 %>%
+dat = data_2019 %>%
   filter(job_function %in% c(3) & worker == 1 & signed_work_card == 2) %>%
   select(job_function, worker, signed_work_card, years_of_study)
 
-l = mean(df$years_of_study)
-ll = sd(df$years_of_study)
+l = mean(dat$years_of_study)
+ll = sd(dat$years_of_study)
 
-df = data_2019 %>%
+dat = data_2019 %>%
   filter(job_function %in% c(3) & worker == 1 & signed_work_card == 2 &
            !is.na(monthly_work_income)) %>%
   select(job_function, worker, signed_work_card, monthly_work_income)
 
-lll = mean(df$monthly_work_income)
-llll = sd(df$monthly_work_income)
+lll = mean(dat$monthly_work_income)
+llll = sd(dat$monthly_work_income)
 
 # Setor Público Formal e Informal
 
-df = data_2019 %>%
+dat = data_2019 %>%
   filter(job_function %in% c(4) & worker == 1 & signed_work_card == 1) %>%
   select(job_function, worker, signed_work_card, years_of_study)
 
-m = mean(df$years_of_study)
-mm = sd(df$years_of_study)
+m = mean(dat$years_of_study)
+mm = sd(dat$years_of_study)
 
-df = data_2019 %>%
+dat = data_2019 %>%
   filter(job_function %in% c(4) & worker == 1 & signed_work_card == 1 &
            !is.na(monthly_work_income)) %>%
   select(job_function, worker, signed_work_card, monthly_work_income)
 
-mmm = mean(df$monthly_work_income)
-mmmm = sd(df$monthly_work_income)
+mmm = mean(dat$monthly_work_income)
+mmmm = sd(dat$monthly_work_income)
 
-df = data_2019 %>%
+dat = data_2019 %>%
   filter(job_function %in% c(4) & worker == 1 & signed_work_card == 2) %>%
   select(job_function, worker, signed_work_card, years_of_study)
 
-n = mean(df$years_of_study)
-nn = sd(df$years_of_study)
+n = mean(dat$years_of_study)
+nn = sd(dat$years_of_study)
 
-df = data_2019 %>%
+dat = data_2019 %>%
   filter(job_function %in% c(4) & worker == 1 & signed_work_card == 2 &
            !is.na(monthly_work_income)) %>%
   select(job_function, worker, signed_work_card, monthly_work_income)
 
-nnn = mean(df$monthly_work_income)
-nnnn = sd(df$monthly_work_income)
+nnn = mean(dat$monthly_work_income)
+nnnn = sd(dat$monthly_work_income)
 
 # Empregadores Formais e Informais
 
-df = data_2019 %>%
+dat = data_2019 %>%
   filter(job_function %in% c(5) & worker == 1 & cnpj == 1) %>%
   select(job_function, worker, cnpj, years_of_study)
 
-o = mean(df$years_of_study)
-oo = sd(df$years_of_study)
+o = mean(dat$years_of_study)
+oo = sd(dat$years_of_study)
 
-df = data_2019 %>%
+dat = data_2019 %>%
   filter(job_function %in% c(5) & worker == 1 & cnpj == 1 &
            !is.na(monthly_work_income)) %>%
   select(job_function, worker, cnpj, monthly_work_income)
 
-ooo = mean(df$monthly_work_income)
-oooo = sd(df$monthly_work_income)
+ooo = mean(dat$monthly_work_income)
+oooo = sd(dat$monthly_work_income)
 
-df = data_2019 %>%
+dat = data_2019 %>%
   filter(job_function %in% c(5) & worker == 1 & cnpj == 2) %>%
   select(job_function, worker, cnpj, years_of_study)
 
-p = mean(df$years_of_study)
-pp = sd(df$years_of_study)
+p = mean(dat$years_of_study)
+pp = sd(dat$years_of_study)
 
-df = data_2019 %>%
+dat = data_2019 %>%
   filter(job_function %in% c(5) & worker == 1 & cnpj == 2 &
            !is.na(monthly_work_income)) %>%
   select(job_function, worker, cnpj, monthly_work_income)
 
-ppp = mean(df$monthly_work_income)
-pppp = sd(df$monthly_work_income)
+ppp = mean(dat$monthly_work_income)
+pppp = sd(dat$monthly_work_income)
 
 # Conta Própria Formal e Informal
 
-df = data_2019 %>%
+dat = data_2019 %>%
   filter(job_function %in% c(6) & worker == 1 & social_security_taxpayer == 1) %>%
   select(job_function, worker, social_security_taxpayer, years_of_study)
 
-r = mean(df$years_of_study)
-rr = sd(df$years_of_study)
+r = mean(dat$years_of_study)
+rr = sd(dat$years_of_study)
 
-df = data_2019 %>%
+dat = data_2019 %>%
   filter(job_function %in% c(6) & worker == 1 & social_security_taxpayer == 1 &
            !is.na(monthly_work_income)) %>%
   select(job_function, worker, social_security_taxpayer, monthly_work_income)
 
-rrr = mean(df$monthly_work_income)
-rrrr = sd(df$monthly_work_income)
+rrr = mean(dat$monthly_work_income)
+rrrr = sd(dat$monthly_work_income)
 
-df = data_2019 %>%
+dat = data_2019 %>%
   filter(job_function %in% c(6) & worker == 1 & social_security_taxpayer == 2) %>%
   select(job_function, worker, social_security_taxpayer, years_of_study)
 
-s = mean(df$years_of_study)
-ss = sd(df$years_of_study)
+s = mean(dat$years_of_study)
+ss = sd(dat$years_of_study)
 
-df = data_2019 %>%
+dat = data_2019 %>%
   filter(job_function %in% c(6) & worker == 1 & social_security_taxpayer == 2 &
            !is.na(monthly_work_income)) %>%
   select(job_function, worker, social_security_taxpayer, monthly_work_income)
 
-sss = mean(df$monthly_work_income)
-ssss = sd(df$monthly_work_income)
+sss = mean(dat$monthly_work_income)
+ssss = sd(dat$monthly_work_income)
 ######################
