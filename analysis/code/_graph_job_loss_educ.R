1a4c45ec4774bdc242ccd6b7b63220355306430c
FredieDidier <fdidier@terra.com.br>
2023-05-19 12:37:42 -0300

    Analysis - Improving graph

--- analysis/code/_graph_job_loss_educ.R
+++ analysis/code/_graph_job_loss_educ.R
@@ -54,16 +54,16 @@ graph = ggplot(df, aes(x = reorder(educ, -job_loss))) +
   geom_bar(aes(y = job_loss, fill = educ_levels), stat = "identity") +
   scale_fill_manual(name = "Education Level",
                     values = carto_pal(name = "Safe")) +
-  labs(x = "", y = "Job Loss %") + ggtitle("Job Loss by Education Level") +
+  labs(x = "", y = "Job Loss %") +
   theme_minimal() +
   theme(text = element_text(family = "Open Sans"),
         plot.title = element_text(size = 18, face = "bold", hjust = 0.5),
         legend.title = element_text(size = 18, face = "bold"), 
         legend.text = element_text(size = 20),
-        legend.position = "right",
+        legend.position = c(0.85,0.85),
         axis.title = element_text(size = 18, face = "bold", hjust = 0.5),
         strip.text = element_text(size = 18, face = "bold", hjust = 0.5),
-        axis.line = element_line(size = 0.75, colour = "black"),
+        axis.line = element_line(linewidth = 0.75, colour = "black"),
         axis.text.x = element_blank(),
         axis.text.y = element_text(
           family = "Helvetica",
@@ -72,3 +72,7 @@ graph = ggplot(df, aes(x = reorder(educ, -job_loss))) +
         )) +
   guides(fill = guide_legend(nrow = 4, byrow = TRUE))
 
+graph
+
+ggsave(filename = here(wd, "analysis", "output", "graph", "_graph_job_loss_educ.png"),
+       width = 9, height = 12, device = "png", dpi = 300)
